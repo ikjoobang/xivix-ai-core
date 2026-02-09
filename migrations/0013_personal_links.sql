9991856ac323fef89ec068d6e2c794364f8e93e1
ikjoobang <ikjoobang@gmail.com>
2026-02-09 08:31:04 +0000

    fix: 프로덕션 마이그레이션 호환성 수정 (0012/0013 no-op)

--- migrations/0013_personal_links.sql
+++ migrations/0013_personal_links.sql
@@ -1,15 +1,5 @@
 -- 개인 SNS/홈페이지 링크 컬럼 추가 (보험설계사용)
 -- 마이그레이션: 0013_personal_links.sql
 -- 생성일: 2026-02-09
-
--- 개인 홈페이지
-ALTER TABLE xivix_stores ADD COLUMN personal_website TEXT;
-
--- 개인 인스타그램
-ALTER TABLE xivix_stores ADD COLUMN personal_instagram TEXT;
-
--- 개인 블로그
-ALTER TABLE xivix_stores ADD COLUMN personal_blog TEXT;
-
--- 개인 유튜브
-ALTER TABLE xivix_stores ADD COLUMN personal_youtube TEXT;
+-- NOTE: 이미 프로덕션에 적용된 경우를 위해 no-op
+SELECT 1;
